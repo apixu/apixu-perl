e562dc7af497c65473499d8ec2c7904800924ff7
Andrei Avram <avramandrei@ymail.com>
2019-02-02 17:45:56 +0200

    Add new methods.

--- APIXU/API/APIXU.pm
+++ APIXU/API/APIXU.pm
@@ -8,9 +8,9 @@ use JSON;
 my $json = JSON->new->allow_nonref;
 my $api_url = 'https://api.apixu.com/v1/';
 my $format = 'json';
+my $weather_conditions_url = 'https://www.apixu.com/doc/Apixu_weather_conditions';
 
-sub new
-{
+sub new {
     my $class = shift;
     my $self = {
         '_api_key' => shift
@@ -19,32 +19,51 @@ sub new
     return $self;
 }
 
+sub conditions {
+    my $url = "$weather_conditions_url.$format";
+    return &get_api_response($url);
+}
+
 sub current {
-    my ($self, $location) = @_;
+    my ($self, $query) = @_;
     my $api_key = $self->{'_api_key'};
-    my $url = $api_url."current.$format?key=$api_key&q=$location";
+    my $url = $api_url."current.$format?key=$api_key&q=$query";
     return &get_api_response($url);
 }
 
 sub forecast {
-    my ($self, $location, $days) = @_;
+    my ($self, $query, $days) = @_;
     my $api_key = $self->{'_api_key'};
-    my $url = $api_url."forecast.$format?key=$api_key&q=$location&days=$days";
+    my $url = $api_url."forecast.$format?key=$api_key&q=$query&days=$days";
+    return &get_api_response($url);
+}
+
+sub history {
+    my ($self, $query, $since) = @_;
+    my $api_key = $self->{'_api_key'};
+    my $url = $api_url."history.$format?key=$api_key&q=$query&dt=$since";
+    return &get_api_response($url);
+}
+
+sub search {
+    my ($self, $query) = @_;
+    my $api_key = $self->{'_api_key'};
+    my $url = $api_url."search.$format?key=$api_key&q=$query";
     return &get_api_response($url);
 }
 
 sub get_api_response {
     my ($url) = @_;
-    my $ua       = LWP::UserAgent->new();
+    my $ua = LWP::UserAgent->new();
     my $response = $ua->get( $url );
 
     if ($response->is_success) {
-        my $json_resp = $response->decoded_content;  # or whatever
+        my $json_resp = $response->decoded_content;
         my $json_decoded = $json->decode( $json_resp );
         return $json_decoded;
     }
     else {
-        my $resp_message = $response->message;  # or whatever
+        my $resp_message = $response->message;
         my %json_resp = (
             'error' => {
                 'message'  => $resp_message,
