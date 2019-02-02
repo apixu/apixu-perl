57029b6cab6291c97a21b63756106582b84d0079
Andrei Avram <avramandrei@ymail.com>
2019-02-02 23:49:22 +0200

    Use constants.

--- APIXU/API/APIXU.pm
+++ APIXU/API/APIXU.pm
@@ -7,11 +7,10 @@ use JSON;
 
 my $json = JSON->new->allow_nonref;
 
-my $api_url = 'https://api.apixu.com/v1/';
-my $format = 'json';
-my $weather_conditions_url = 'https://www.apixu.com/doc/Apixu_weather_conditions';
-
 use constant {
+    API_URL => 'https://api.apixu.com/v1/',
+    FORMAT => 'json',
+    WEATHER_CONDITIONS_URL => 'https://www.apixu.com/doc/Apixu_weather_conditions',
     HTTP_TIMEOUT => 20,
     HTTP_STATUS_INTERNAL_SERVER_ERROR => 500,
 };
@@ -26,35 +25,35 @@ sub new {
 }
 
 sub conditions {
-    my $url = "$weather_conditions_url.$format";
+    my $url = WEATHER_CONDITIONS_URL.".".FORMAT;
     return &get_api_response($url);
 }
 
 sub current {
     my ($self, $query) = @_;
     my $api_key = $self->{'_api_key'};
-    my $url = $api_url."current.$format?key=$api_key&q=$query";
+    my $url = API_URL."current.".FORMAT."?key=$api_key&q=$query";
     return &get_api_response($url);
 }
 
 sub forecast {
     my ($self, $query, $days) = @_;
     my $api_key = $self->{'_api_key'};
-    my $url = $api_url."forecast.$format?key=$api_key&q=$query&days=$days";
+    my $url = API_URL."forecast.".FORMAT."?key=$api_key&q=$query&days=$days";
     return &get_api_response($url);
 }
 
 sub history {
     my ($self, $query, $since) = @_;
     my $api_key = $self->{'_api_key'};
-    my $url = $api_url."history.$format?key=$api_key&q=$query&dt=$since";
+    my $url = API_URL."history.".FORMAT."?key=$api_key&q=$query&dt=$since";
     return &get_api_response($url);
 }
 
 sub search {
     my ($self, $query) = @_;
     my $api_key = $self->{'_api_key'};
-    my $url = $api_url."search.$format?key=$api_key&q=$query";
+    my $url = API_URL."search.".FORMAT."?key=$api_key&q=$query";
     return &get_api_response($url);
 }
 
